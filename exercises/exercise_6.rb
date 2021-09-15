9890f9c62f86641d88a48226637991831f4c37d3
Ahmed <achau1019@gmail.com>
2021-09-15 00:38:07 +0000

    Define relationships, add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Ahmed", last_name: "Chaudry", hourly_rate: 55)
+@store1.employees.create(first_name: "Ali", last_name: "Cheema", hourly_rate: 45)
+@store1.employees.create(first_name: "Patrick", last_name: "Agnew", hourly_rate: 55)
